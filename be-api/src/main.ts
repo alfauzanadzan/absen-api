2991b604c643238c92c6ff01aa3cea717b64f8fa
alzanmizam <alfauzanadzan81@gmail.com>
2025-09-08 10:03:04 +0700

    wa2

--- be-api/src/main.ts
+++ be-api/src/main.ts
@@ -5,6 +5,13 @@ import { AuthService } from './auth/auth.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // ✅ Enable CORS biar Nuxt (http://localhost:3001) bisa call API (http://localhost:3000)
+  app.enableCors({
+    origin: 'http://localhost:3001', // FE Nuxt
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   // 🔹 Reset / seed superadmin tiap kali start
   const authService = app.get(AuthService);
   await authService.seedSuperAdmin();
